0898ac8d936fa8780aba4d233be24e4c90cc9ba2
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-12-19 18:27:21 +0100

    Adds movement to car

--- Assets/Scripts/Vehicle/CarMovement.cs
+++ Assets/Scripts/Vehicle/CarMovement.cs
@@ -6,20 +6,54 @@ namespace Vehicle{
     [RequireComponent(typeof(Rigidbody))]
     public class CarMovement : MonoBehaviour{
         [SerializeField] CarSo carSo;
-        ICarHandler carHandler; 
+        ICarHandler carHandler;
+        [SerializeField]bool onGround;
+        [SerializeField] float raycastLenght;
+        RaycastHit hit;
+        float sideSpeed;
+        float forwardSpeed;
+        float startAngularDrag;
         Rigidbody rb => GetComponent<Rigidbody>();
 
         void Awake(){
             carHandler = GetComponent<ICarHandler>();
         }
 
-        void FixedUpdate(){
-            OnMove(carHandler.Direction);
+        void Start(){
+            startAngularDrag = rb.angularDrag;
         }
+
+        void FixedUpdate(){
+            forwardSpeed = Vector3.Dot(rb.velocity, transform.forward);
+            sideSpeed = Vector3.Dot(rb.velocity, transform.right);
+            rb.AddForce((-sideSpeed * transform.right), ForceMode.VelocityChange);
+            OnMove(carHandler.Direction);
+            //TODO: Add on ticks return!
+            OnGround();
+        }
+
         void OnMove(Vector2 direction){
             //MaxSpeed
-            rb.AddForce(transform.forward * carHandler.Direction.x * carSo.Speed);
-            
+            if (!onGround)
+                return;
+            if (forwardSpeed == Mathf.Clamp(forwardSpeed, -10, 25))
+                rb.AddForce(transform.forward * carHandler.Direction.x * carSo.Speed, ForceMode.Acceleration);
+            var turningSpeed = (forwardSpeed * 0.5f) * carHandler.Direction.y;
+            rb.AddTorque(transform.up * turningSpeed * carSo.TurnSpeed, ForceMode.Acceleration);
+
+        }
+
+        void OnGround(){
+            Debug.DrawRay(transform.position,-transform.up * raycastLenght);
+            if (Physics.Raycast(transform.position, -transform.up, out hit, raycastLenght,1 << LayerMask.NameToLayer("Default"))){
+                onGround = true;
+                rb.angularDrag= startAngularDrag;
+            }
+            else{
+                onGround = false;
+                rb.angularDrag = 0;
+            }
+                
         }
     }
 }
--- Assets/Scripts/Vehicle/CarSo.cs
+++ Assets/Scripts/Vehicle/CarSo.cs
@@ -6,9 +6,9 @@ namespace Vehicle{
     public class CarSo : ScriptableObject{
         [SerializeField] float speed;
         [SerializeField] float turnSpeed;
-        [SerializeField] float handling;
+        [SerializeField] float gripOnSpeed;
         public float Speed => speed;
-
         public float TurnSpeed => turnSpeed;
+        public float GripOnSpeed => gripOnSpeed;
     }
 }
