cbc07f4273d74852cc6a21a32c15bebf34ee6f69
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2020-12-20 12:09:53 +0100

    Uppdate ui

---
+++ Assets/Scripts/DeathHazard.cs
@@ -0,0 +1,15 @@
+﻿using Patient;
+using UnityEngine;
+
+public class DeathHazard : MonoBehaviour{
+    [SerializeField] StringEvent gameOverEvent;
+
+    void GameOver(){
+        gameOverEvent?.Invoke("Mission failed!");
+    }
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")){
+            Invoke(nameof(GameOver),1);
+        }
+    }
+}
